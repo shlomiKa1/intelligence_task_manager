1b552457d7b2109a5b2a99b10c44b659aa61f1be
Shlomi <shlomikaufman91@gmail.com>
2026-06-17 11:47:39 +0300

    Add CRUD on agent_db

---
+++ database/agent_db.py
@@ -0,0 +1,19 @@
+from .db_connection import db
+from .base_db import BaseDB
+
+
+class AgentDB(BaseDB):
+    def __init__(self, db, table_name):
+        super().__init__(db, table_name)
+    
+    def create_agent(self, data):
+        return self.create(data)
+    
+    def update_agent(self, id, data):
+        return self.update(id, data)
+    
+    def get_all_agents(self):
+        return self.get_all()
+    
+    def get_agent_by_id(self, id):
+        return self.get_by_id(id)
