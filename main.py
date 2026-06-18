e5f05b8d4b7e5145a2d392a1ca2da114cc6eb022
Shlomi <shlomikaufman91@gmail.com>
2026-06-18 16:44:19 +0300

    Add main file & Fix Bugs

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from routes.mission_routes import router_missions
 from routes.report_routes import router_reports
 from database.db_connection import db
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from logs.logger_config import logger
 
 
 @asynccontextmanager
@@ -13,6 +14,13 @@ async def lifespan(app: FastAPI):
     db.connection.close()
 
 app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def add_logs(request: Request, call_next):
+     logger.info("%s %s", request.method, request.url)
+     response = await call_next(request)
+
+     return response
 
 app.include_router(router=router_agents, prefix="/agents", tags=["Agents"])
 app.include_router(router=router_missions, prefix="/missions", tags=["Missions"])
