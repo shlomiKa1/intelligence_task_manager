c88bba5ac3f00961347f762057eb3566e4d7f60b
Shlomi <shlomikaufman91@gmail.com>
2026-06-18 10:44:58 +0300

    Add route of create agent

---
+++ routes/agent_routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, HTTPException
+from database.agent_db import agents_db
+from logs.logger_config import logger
+
+router_agents = APIRouter()
+
+@router_agents.post("")
+def add_agent(data: dict):
+    logger.info("Go to database")
+    added = agents_db.create_agent(data)
+
+    if added["success"]:
+        raise HTTPException(500, "Internal Server Error")
+    logger.info("Agent created successfully: id=%s", added["id"])
+    return added
+
